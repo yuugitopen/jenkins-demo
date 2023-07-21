3da731c695344c62c1ae383a14d4ef8a7f2662a1
Yuvaraj Selvaraj <yuvaraj.s@gjglobalsoft.com>
2023-07-21 12:39:51 +0530

    Sample Spring Boot App for Jenkins Build

---
+++ Maven-SpringBoot-Demo/src/main/java/com/yuu/demo/MavenSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yuu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenSpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ Maven-SpringBoot-Demo/src/test/java/com/yuu/demo/mavenspringbootdemo/MavenSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuu.demo.mavenspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
